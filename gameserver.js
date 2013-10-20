85869a1a601e344ccef9a36746a85ce12d0ed1e4
Philip Roberts <phil@andyet.net>
2013-10-20 14:22:01 -0700

    Simple game

--- gameserver.js
+++ gameserver.js
@@ -1,10 +1,28 @@
 var Channel = require('./channel.js')
+var $ = require('jquery-browserify')
+window.$ = $;
 
 var channel = new Channel({
   roomName: 'foo'
 });
 
 
-channel.on('button:press', function(data) {
-  console.log("We got a ", data, "button press for ");
+$(function() {
+  var $ball = $('.ball');
+
+  channel.on('button:press', function(data) {
+    if (data.button == "up")
+      $ball.css({ top: -10 + parseInt($ball.css('top'), 10) })
+
+    if (data.button == "down")
+      $ball.css({ top: 10 + parseInt($ball.css('top'), 10) })
+
+    if (data.button == "left")
+      $ball.css({ left: -10 + parseInt($ball.css('left'), 10) })
+
+    if (data.button == "right")
+      $ball.css({ left: 10 + parseInt($ball.css('left'), 10) })
+
+    console.log("We got a ", data, "button press for ");
+  });
 });
